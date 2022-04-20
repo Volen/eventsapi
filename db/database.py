5055329bbd47f08752207f6d14bdae01f18f1e13
Vasiliy Voyakin <voyakin@edsd.com>
2022-04-20 12:36:58 +0300

    Hide secret key.

--- db/database.py
+++ db/database.py
@@ -1,6 +1,5 @@
 from deta import Deta
 
-def get_deta():
-    # remove secret key before deploy
-    deta = Deta("c0b313oq_QuVjFdj29QPY5mrfyi9QVwF49B4g1Syu")
+def get_deta():    
+    deta = Deta(ENV['DETA_API'])
     return deta
